4f302613e97bd80a160ae2fec11156d06b8af079
Alberto Pascau <tito@Albertos-MacBook-Air.local>
2025-03-22 17:28:43 +0100

    Change for dump and zsh history

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ stow -v .
 stow -v --target=$HOME tmux
 
 # Set up zsh path to config
+echo "export HISTFILE="$HOME/.local/share/zsh/history"" >> $HOME/.zshenv
+echo "export ZSH_COMPDUMP="$HOME/.cache/zsh/zcompdump"" >> $HOME/.zshenv
 echo "export ZDOTDIR="$HOME/.config/zshrc"" >> $HOME/.zshenv
