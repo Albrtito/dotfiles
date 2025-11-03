fcefb1b50a32e6dbe590fa2b38710731a7797b32
tito <alb.pascau@gmail.com>
2025-11-03 21:03:21 +0000

    feat/update font in pandoc script to JetBrainsMono

--- scripts/pandoc.sh
+++ scripts/pandoc.sh
@@ -2,7 +2,7 @@
 
 # Meant to be called from the location where the .md file is
 pandoc report.md -o report.pdf --pdf-engine=xelatex --from markdown+tex_math_dollars -V mainfont="Iosevka" \
-  -V monofont="Iosevka"
+  -V monofont="JetBrainsMono"
 if ! lsof report.pdf >/dev/null 2>&1; then
   open report.pdf
 fi
